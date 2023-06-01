385e68d5b600abb5d89ab9abb35eeb8c0798bec4
Neptunal <110062414+nptnl@users.noreply.github.com>
2023-06-01 13:53:26 -0400

    Initial commit

---
+++ count.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x = 0;
+    while (x < 50) {
+        x += 1;
+    }
+    printf("{}", x);
+    return 0;
+}
---
+++ henlo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("henlo\n");
+    printf("i dont like C");
+    return 0;
+}
